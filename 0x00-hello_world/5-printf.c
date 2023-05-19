2d488805f2573fe2a1311ef77447682bad852818
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-18 17:54:50 -0700

    whitespace and \n

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - function that contains printf
- * Return: returns 0 
+ * Return: returns 0
  */
 int main(void)
 {
-	printf("with proper grammr, but the outcome is a piece of art,");
+	printf("with proper grammr, but the outcome is a piece of art,\n");
 	return (0);
 }
