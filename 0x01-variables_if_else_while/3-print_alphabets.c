b8460d6b429492e249d4f84ab081581760d05973
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-19 16:42:00 -0700

    yanked

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet
+ * Return: always 0
+ */
+int main(void)
+{
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (int i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
