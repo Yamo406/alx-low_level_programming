2200ff8f4d326afdd2d6ee1b7b36c574d4dd6708
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-18 17:53:17 -0700

    printf betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function that contains printf
+ * Return: returns 0 
+ */
+int main(void)
+{
+	printf("with proper grammr, but the outcome is a piece of art,");
+	return (0);
+}
