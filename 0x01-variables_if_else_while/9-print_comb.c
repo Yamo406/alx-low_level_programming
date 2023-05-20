f295458310a130c5bf9a19d534875079e4d6b2de
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-19 17:44:29 -0700

    what was I thinking

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
+		if (i == 57)
+			continue;
 		putchar(44);
 		putchar(32);
 	}
-	putchar(57);
+	putchar(10);
 	return (0);
 }
