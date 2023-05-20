42a61126866e8ca004e6875b8924e53758909956
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-19 17:09:55 -0700

    putchar numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
