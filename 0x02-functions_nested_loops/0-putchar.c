934e1529649de8ec1d60ce289118c393d90165d0
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-23 15:56:42 -0700

    add single quotes for the chars

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include "main.h"
 /**
   * main - main function to print _putchar
   * Return: return 0
   */
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
