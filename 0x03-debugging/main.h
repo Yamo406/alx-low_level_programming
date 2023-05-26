42b5dce23eb6cc2c4ed283c648f2b182c5502e1b
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-26 16:27:14 -0700

    largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,20 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i)
-{
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
-}
+
 #endif /* MAIN_H */
