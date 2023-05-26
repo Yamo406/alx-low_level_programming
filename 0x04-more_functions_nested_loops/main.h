cf8258f1893738d492953b9d9027dc7314a8bc6a
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-26 16:50:54 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int c)
+{
+	if (c > 65 && c < 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+#endif /* MAIN_H */
