c4730a8e729a09b0bfb6b76d19609f0be2ee77f3
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-25 17:13:37 -0700

    check 4 error

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%i is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%i is negative\n", i);
+	}
+	else
+	{
+		printf("%i is zero\n", i);
+	}
+}
 #endif /* MAIN_H */
