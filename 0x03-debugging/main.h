5b5806f91bb79ad525c412abf355b989fbc12ae1
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-25 17:10:59 -0700

    0 now

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,20 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i)
-{
-	if (i > 0)
-	{
-		printf("%i is positive\n", i);
-	}
-	else if (i < 0)
-	{
-		printf("%i is negative\n", i);
-	}
-	else
-	{
-		printf("%i is zero\n", i);
-	}
-}
+void positive_or_negative(int i);
 
 #endif /* MAIN_H */
