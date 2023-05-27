145a0eab8f79fa87726cdb38b1c8cbaf9991f2c2
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-26 18:23:14 -0700

    multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiple function
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int result = 0;
+
+	result = a * b;
+
+	return result;
+}
