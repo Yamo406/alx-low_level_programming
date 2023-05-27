00280abc356ba8202ee7f27f5f4323a9e5b5de04
kdayamo <mohamadd.abdelnaby@gmail.com>
2023-05-26 18:39:06 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * numbers - print numberss function
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
